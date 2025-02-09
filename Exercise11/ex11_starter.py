08064ca9313a37d5ec92a32dd92518d46c1239bc
Aiman Ahmed <aimanahmed61@gmail.com>
2025-02-09 00:29:40 +0000

    EDIT: completed ex11_starter.py activity

--- Exercise11/ex11_starter.py
+++ Exercise11/ex11_starter.py
@@ -2,3 +2,26 @@
 
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
 
+print(Belgium)
+
+print (len(Belgium))
+
+print('-'* 81)
+
+# print(Belgium, sep=':')
+
+# separate = Belgium.split(':')
+#
+# print(separate)
+
+print('Belgium','10445852','Brussels','737966','Europe','1830','Euro','Catholicism','Dutch','French','German', sep=':')
+
+list_Belgium= ['Belgium','10445852','Brussels','737966','Europe','1830','Euro','Catholicism','Dutch','French','German']
+print(list_Belgium)
+print(':'.join(list_Belgium))
+
+print(list_Belgium[1])
+print(list_Belgium[3])
+
+Calaculation = int(list_Belgium[1]) + int(list_Belgium[3])
+print(Calaculation)
